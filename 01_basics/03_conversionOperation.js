8dc88febdb6df383417b8613c5ce1622e777fc89
Rabin Regmi <134535838+rabin342@users.noreply.github.com>
2025-05-27 09:04:25 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "Rabin"
+
+//console.log(score); 
+//console.log(typeof (score));
+let valueInNUmber = Number(score)
+//console.log(valueInNUmber);
+//console.log(typeof valueInNUmber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+ let isLoggedIn = "Rabiiin"
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ //console.log(booleanIsLoggedIn);
+ // 1 => true ; 0 => false
+ // "" => false
+ // "Rabiin" => true
+
+ let someNumber = 3232
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
+ 
+
+ 
+
+
+
