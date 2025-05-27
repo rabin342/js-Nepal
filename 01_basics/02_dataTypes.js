de035603cac9d01a52454e7c78cc7dab07a2e11d
Rabin Regmi <134535838+rabin342@users.noreply.github.com>
2025-05-27 07:18:29 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use struct"; //treat all js code as newer version
+
+// alert( 3 + 3 ) // we. are using nodejs, not browser
+console.log(3 + 3) // code readability should be high
+console.log("rabin");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state; //undefined
+
+
+// number => 2 to power 53
+//bigint => facebook.stock market
+//string => "rab"
+//bolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+console.log(typeof "123");
+console.log(typeof "wer");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
+
